16e007c5bd1c8af45b427244053905a95e8837f5
DevRajeev <rjvranjan80@gmail.com>
2020-06-26 23:42:55 +0530

    Validation, Success Error Notification & Model Preparation Done

--- src/app/r-teacher/add-course/add-course.page.ts
+++ src/app/r-teacher/add-course/add-course.page.ts
@@ -20,6 +20,12 @@ export class AddCoursePage implements OnInit {
   chapterQuery : any;
   topicQuery : any;
   paragraphQuery : any;
+  selectedSection : string;
+  selectedChapter : string;
+  selectedTopic : string;
+  selectedParagraph : string;
+  validationError : any= { formValid :true, validationField:'', errorMessage:''};
+  successNotification : any = { visible : false, successMessage :''};
 
   constructor() {}
 
@@ -36,13 +42,22 @@ export class AddCoursePage implements OnInit {
       topic: "",
       paragraph: "",
       supportingDocs: "",
+      sectionDropdown:"",
+      chapterDropdown : "",
+      topicDropdown : "",
+      paragraphDropdown : "",
+      sectionDetails: {}
     };
   }
-
+  
   onAddSection(sec) {
     if (sec === "") {
+      this.validationError.formField = "Section";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"     
       return;
     }
+    this.validationError.formValid = true;
     let count = this.sectionList.length;
     if (count == 0) {
       this.sectionList.push({ id: 1, sectionName: sec });
@@ -51,24 +66,35 @@ export class AddCoursePage implements OnInit {
     }
     let json = { section: [{ sectionName: sec, chapter: [] }] };
     this.sectionJSON.push(json);
-    alert("added");
+    this.successNotification.visible=true;
+    this.successNotification.successMessage = "Section added.";
   }
 
   onSectionChange(value) {
+    this.selectedSection = value;
     this.sectionQuery = this.sectionJSON.find(
       (a) => a.section[0].sectionName == value
     );
   }
 
   onAddChapter(value) {
-    if (value === "") {
+     if(this.courseModel.sectionDropdown==''){
+      this.validationError.formField = "Choose Section";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"
+      return;
+    } else if (value === "") {
+      this.validationError.formField = "Chapter Name";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"
       return;
     }
+    this.validationError.formValid = true;
     let count = this.chapterList.length;
     if (count == 0) {
-      this.chapterList.push({ id: 1, chapterName: value });
+      this.chapterList.push({ id: 1, chapterName: value, sectionName : this.selectedSection });
     } else {
-      this.chapterList.push({ id: count + 1, chapterName: value });
+      this.chapterList.push({ id: count + 1, chapterName: value, sectionName : this.selectedSection });
     }
     if (this.sectionQuery != null) {
       let indexSection = this.sectionJSON.indexOf(this.sectionQuery);
@@ -77,22 +103,34 @@ export class AddCoursePage implements OnInit {
         topic: [],
       });
     }
-    alert("added");
+    this.successNotification.visible=true;
+    this.successNotification.successMessage = "Chapter added.";
   }
 
   onChapterChange(value) {
+    this.selectedChapter = value;
     this.chapterQuery = this.sectionQuery.section[0].chapter.find( (a) => a.chapterName == value);
   }
 
   onAddTopic(value) {
-    if (value === "") {
+    if(this.courseModel.chapterDropdown==''){
+      this.validationError.formField = "Choose Chapter";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"
       return;
     }
+    else if (value === "") {
+      this.validationError.formField = "Topic";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"
+      return;
+    }
+    this.validationError.formValid = true;
     let count = this.topicList.length;
     if (count == 0) {
-      this.topicList.push({ id: 1, topicName: value });
+      this.topicList.push({ id: 1, topicName: value, sectionName : this.selectedTopic, chapterName : this.selectedChapter });
     } else {
-      this.topicList.push({ id: count + 1, topicName: value });
+      this.topicList.push({ id: count + 1, topicName: value, sectionName : this.selectedTopic, chapterName : this.selectedChapter });
     }
     if (this.chapterQuery != null) {
       this.chapterQuery.topic.push({
@@ -100,22 +138,34 @@ export class AddCoursePage implements OnInit {
         paragraph: [],
       });
     }
-    alert("added");
+    this.successNotification.visible=true;
+    this.successNotification.successMessage = "Topic added.";
   }
 
   onTopicChange(value) {
+    this.selectedTopic = value;
     this.topicQuery = this.chapterQuery.topic.find( (a) => a.topicName == value);
   }
 
   onAddParagraph(value) {
-    if (value === "") {
+    if(this.courseModel.topicDropdown==''){
+      this.validationError.formField = "Choose Topic";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"
       return;
     }
+    else if (value === "") {
+      this.validationError.formField = "Paragraph";
+      this.validationError.formValid = false;
+      this.validationError.errorMessage = " is required"
+      return;
+    }
+    this.validationError.formValid = true;
     let count = this.paragraphList.length;
     if (count == 0) {
-      this.paragraphList.push({ id: 1, paragraphName: value });
+      this.paragraphList.push({ id: 1, paragraphName: value, sectionName : this.selectedTopic, chapterName : this.selectedChapter, topicName: this.selectedTopic });
     } else {
-      this.paragraphList.push({ id: count + 1, paragraphName: value });
+      this.paragraphList.push({ id: count + 1, paragraphName: value, sectionName : this.selectedTopic, chapterName : this.selectedChapter, topicName: this.selectedTopic });
     }
     if (this.topicQuery != null) {
       this.topicQuery.paragraph.push({
@@ -123,41 +173,18 @@ export class AddCoursePage implements OnInit {
         supportingDocs: '',
       });
     }
-    alert("added");
+    this.successNotification.visible=true;
+    this.successNotification.successMessage = "Paragraph added.";
   }
 
   onParagraphChange(value) {
-    console.log(value);
+   this.selectedParagraph = value;
   }
 
-  prepareModel() {
-    let model = {} as AddCourseModel;
+  onSubmit(){
+    this.courseModel.sectionDetails = this.sectionJSON;
+    console.log(this.courseModel);
   }
-  // public blocks: any[] = [
-  //   {
-  //     section: [
-  //       {
-  //         sectionName: "1",
-  //         chapter: [
-  //           {
-  //             chapterName: "1",
-  //             topic: [
-  //               {
-  //                 topicName: "some topic",
-  //                 paragraph: [
-  //                   {
-  //                     paragraphName: "some pargraph",
-  //                     supportingDocs: "",
-  //                   },
-  //                 ],
-  //               },
-  //             ],
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // ];
 }
 
 //{"section":[{"sectionName":"1","chapter":[{"chapterName":"1","topic":[{"topicName":"some topic","paragraph":[{"paragraphName":"some pargraph","supportingDocs":""}]}]}]}]}";"
