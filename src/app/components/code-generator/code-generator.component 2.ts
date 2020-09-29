3788d5fc67f68df1ab944805a4120acbcc916a01
georges tchianga <georgestcm@gmail.com>
2020-09-29 05:07:22 -0400

    Feature: add code generator user addition modal

---
+++ src/app/components/code-generator/code-generator.component 2.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-generator',
+  templateUrl: './code-generator.component.html',
+  styleUrls: ['./code-generator.component.scss'],
+})
+export class CodeGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
