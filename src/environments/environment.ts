8c0862ad9d8f8f8ebcb7d331962a7b9817819621
georges tchianga <georgestcm@gmail.com>
2020-09-29 05:02:23 -0400

    FIX: fix api local routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3000/',
   imageUrl: 'http://localhost:3000/uploads',
   presetPassword :"12345"
 };
